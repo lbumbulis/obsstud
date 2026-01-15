7d95c0a8c71d86a947d1b72107720fbd3da93222
lbumbulis <lbumbulis@gmail.com>
2026-01-15 15:06:09 -0500

    Re-factoring simulation parameter settings and adding back covariate for cumulative exposure that declines with time since quit.

--- with-symptoms/cumulative/datagen.R
+++ with-symptoms/cumulative/datagen.R
@@ -92,19 +92,17 @@ get.first.exit.state <- Vectorize(function(exit.time, v) {
 })
 
 # This function applies to transitions after the first transition out of (0^\circ, 0)
-get.exit.info <- function(j, max.idx, cc, b.start, v, state, method) {
+get.exit.info <- function(j, max.idx, cc, b.start, v, state) {
   E <- get.E.for.state(state)
   Z <- get.Z.for.state(state)
   
-  x6 <- as.numeric(Z==10)
+  x4 <- as.numeric(Z==10)
   if (E==-1) {
-    x <- c(rep(0, nvar-1), x6)
+    x <- c(rep(0, nvar-1), x4)
   } else if (E==1) {
-    x <- c(1, NA, rep(0, nvar-3), x6)
-  } else if (E==0 && Z==0) {
-    x <- c(0, 0, 1, rep(NA, RB), 0, x6)
-  } else if (E==0 && Z==10) {
-    x <- c(0, 0, 1, rep(0, RB), cc*time.scale, x6)
+    x <- c(1, 0, NA, rep(0, RB), x4)
+  } else if (E==0) {
+    x <- c(0, 1, 0, rep(NA, RB), x4)
   }
   
   surv.prob <- runif(1)
@@ -114,14 +112,14 @@ get.exit.info <- function(j, max.idx, cc, b.start, v, state, method) {
   integrand.2 <- Vectorize(function(u) { lam2.fn(u) * exp(beta2[2]*u*time.scale) })
   
   tt <- (j-1)/J
-  get.x4.idx <- function(u) {
+  get.x3.idx <- function(u) {
     b <- b.start + u - tt # time since most recent quit
     return(findInterval(b, b.breaks))
   }
   
-  integrand.1p.b <- Vectorize(function(u) { lam1p.fn(u) * exp(beta1p[3+get.x4.idx(u)]*cc*time.scale) })
-  integrand.1.b <- Vectorize(function(u) { lam1.fn(u) * exp(beta1[3+get.x4.idx(u)]*cc*time.scale) })
-  integrand.2.b <- Vectorize(function(u) { lam2.fn(u) * exp(beta2[3+get.x4.idx(u)]*cc*time.scale) })
+  integrand.1p.b <- Vectorize(function(u) { lam1p.fn(u) * exp(beta1p[3+get.x3.idx(u)]*cc*time.scale) })
+  integrand.1.b <- Vectorize(function(u) { lam1.fn(u) * exp(beta1[3+get.x3.idx(u)]*cc*time.scale) })
+  integrand.2.b <- Vectorize(function(u) { lam2.fn(u) * exp(beta2[3+get.x3.idx(u)]*cc*time.scale) })
   
   # Determine exit time
   if (E==-1 && Z==10) {
@@ -133,11 +131,11 @@ get.exit.info <- function(j, max.idx, cc, b.start, v, state, method) {
   } else if (E==1 && Z==0) {
     uniroot.fn <- function(s) {
       exp(-(
-        exp(beta1p[1] + (cc-tt)*time.scale*beta1p[2] + gamma1p*v) *
+        exp(beta1p[1] + (cc-tt)*time.scale*beta1p[3] + gamma1p*v) *
           integrate(integrand.1p, lower=tt, upper=tt+s)$value +
-          exp(beta1[1] + (cc-tt)*time.scale*beta1[2] + gamma1*v) *
+          exp(beta1[1] + (cc-tt)*time.scale*beta1[3] + gamma1*v) *
           integrate(integrand.1, lower=tt, upper=tt+s)$value +
-          exp(beta2[1] + (cc-tt)*time.scale*beta2[2] + gamma2*v) *
+          exp(beta2[1] + (cc-tt)*time.scale*beta2[3] + gamma2*v) *
           integrate(integrand.2, lower=tt, upper=tt+s)$value +
           s * psi0*exp(eta0*v)
       )) - surv.prob
@@ -145,7 +143,7 @@ get.exit.info <- function(j, max.idx, cc, b.start, v, state, method) {
   } else if (E==1 && Z==10) {
     uniroot.fn <- function(s) {
       exp(-(
-        exp(beta1[1] + (cc-tt)*time.scale*beta1[2] + beta1[nvar] + gamma1*v) *
+        exp(beta1[1] + (cc-tt)*time.scale*beta1[3] + beta1[nvar] + gamma1*v) *
           integrate(integrand.1, lower=tt, upper=tt+s)$value +
           s * psi0*exp(alpha0[nvar] + eta0*v)
       )) - surv.prob
@@ -153,17 +151,16 @@ get.exit.info <- function(j, max.idx, cc, b.start, v, state, method) {
   } else if (E==0 && Z==0) {
     uniroot.fn <- function(s) {
       exp(-(
-        exp(beta1p[3] + gamma1p*v) * integrate(integrand.1p.b, lower=tt, upper=tt+s)$value +
-          exp(beta1[3] + gamma1*v) * integrate(integrand.1.b, lower=tt, upper=tt+s)$value +
-          exp(beta2[3] + gamma2*v) * integrate(integrand.2.b, lower=tt, upper=tt+s)$value +
+        exp(beta1p[2] + gamma1p*v) * integrate(integrand.1p.b, lower=tt, upper=tt+s)$value +
+          exp(beta1[2] + gamma1*v) * integrate(integrand.1.b, lower=tt, upper=tt+s)$value +
+          exp(beta2[2] + gamma2*v) * integrate(integrand.2.b, lower=tt, upper=tt+s)$value +
           s * psi1*exp(eta1*v)
       )) - surv.prob
     }
   } else if (E==0 && Z==10) {
     uniroot.fn <- function(s) {
       exp(-(
-        exp(beta1[3] + cc*time.scale*beta1[nvar-1] + beta1[nvar] + gamma1*v) *
-          integrate(lam1.fn, lower=tt, upper=tt+s)$value
+        exp(beta1[2] + beta1[nvar] + gamma1*v) * integrate(integrand.1.b, lower=tt, upper=tt+s)$value
       )) - surv.prob
     }
   }
@@ -178,14 +175,14 @@ get.exit.info <- function(j, max.idx, cc, b.start, v, state, method) {
     
     # Determine exit state
     if (E==1) {
-      x[2] <- (cc + exit.time - tt) * time.scale
-    } else if (E==0 && Z==0) {
-      r <- get.x4.idx(exit.time)
-      x4 <- rep(0, RB)
+      x[3] <- (cc + exit.time - tt) * time.scale # x_{12}
+    } else if (E==0) {
+      r <- get.x3.idx(exit.time)
+      x3 <- rep(0, RB)
       if (r <= RB) {
-        x4[r] <- cc*time.scale
+        x3[r] <- cc*time.scale
       }
-      x[(1:RB)+3] <- x4
+      x[(1:RB)+3] <- x3
     }
     
     P <- P.fn(exit.time, x, v)
@@ -235,9 +232,9 @@ generate.data <- function(n=nn, print.increment=100) {
   
   print(paste0(Sys.time(), ": Generating follow-up data ..."))
   dat <- as.data.frame(data.table::rbindlist(lapply(i.select, function(i) {
-    if (i==1 || i %% print.increment == 0) {
-      print(paste0(Sys.time(), ": ", i))
-    }
+    if (i %% print.increment == 0) { print(paste0(Sys.time(), ": ", i)) }
+    
+    # saveRDS(.Random.seed, "save_seed.rds")
     
     max.idx <- max.idx.full[i]
     v <- v.full[i]
@@ -285,10 +282,9 @@ generate.data <- function(n=nn, print.increment=100) {
       # } else {
       #   is.select <- T
       # }
-      is.select <- T
       
       while (Z %in% c(0,10) && j <= max.idx) { # Z=10 means Z=1'
-        exit.info <- get.exit.info(j, max.idx, cc, b, v, state, method)
+        exit.info <- get.exit.info(j, max.idx, cc, b, v, state)
         exit.idx <- exit.info$exit.idx
         
         if (!exit.info$is.exit) {
@@ -333,17 +329,17 @@ generate.data <- function(n=nn, print.increment=100) {
           j <- exit.idx + 1
         }
         
-        # Check state occupied at selection time
-        if (!is.select && j-1 >= select.j) {
-          select.state <- dat.i$state[select.j]
-          
-          if (select.state == 5) { # (1,0)
-            is.select <- ifelse(p1==1, T, as.logical(rbinom(n=1, size=1, prob=p1)))
-          }
-          if (!is.select) { return(NULL) }
-        }
+        # # Check state occupied at selection time
+        # if (!is.select && j-1 >= select.j) {
+        #   select.state <- dat.i$state[select.j]
+        #   
+        #   if (select.state == 5) { # (1,0)
+        #     is.select <- ifelse(p1==1, T, as.logical(rbinom(n=1, size=1, prob=p1)))
+        #   }
+        #   if (!is.select) { return(NULL) }
+        # }
       }
-      if (!is.select) { return(NULL) }
+      # if (!is.select) { return(NULL) }
       
       # Current state is never exited; fill in remaining times accordingly
       if (j <= max.idx) {
@@ -373,15 +369,10 @@ generate.data <- function(n=nn, print.increment=100) {
   dat$Z1 <- as.numeric(dat$Z==1)
   dat$Z2 <- as.numeric(dat$Z==2)
   
+  dat$x1 <- factor(as.character(dat$E.prev), levels=c(-1,1,0))
   dat$x2 <- ifelse(dat$E.prev == 1, dat$c*time.scale, 0)
-  # dat.x4 <- as.data.frame(sapply(1:RB, function(r) {
-  #   ifelse(dat$E.prev==0 & dat$Z.prev==0 & dat$B==r, dat$c*time.scale, 0)
-  # }))
-  # names(dat.x4) <- paste0("x4.", 1:RB)
-  # dat <- cbind(dat, dat.x4)
-  dat$x4 <- ifelse(dat$E.prev == 0 & dat$Z.prev == 0, dat$c*time.scale, 0)
-  dat$x5 <- ifelse(dat$E.prev == 0 & dat$Z.prev == 10, dat$c*time.scale, 0)
-  dat$x6 <- as.numeric(dat$Z.prev == 10)
+  dat$x3 <- ifelse(dat$E.prev == 0, dat$c*time.scale, 0)
+  dat$x4 <- as.numeric(dat$Z.prev == 10)
   
   return(dat)
 }
